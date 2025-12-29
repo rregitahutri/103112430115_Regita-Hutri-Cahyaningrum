dc9dde22d8dbf59c721c1c890d61444782944429
Regita Hutri Cahyaningrum <541211147@student.smktelkom-pwt.sch.id>
2025-12-29 09:33:38 +0700

    poin tambahan uts (bukan lapraak)

---
+++ uts/stackGame.h
@@ -0,0 +1,22 @@
+#ifndef STACKGAME_H
+#define STACKGAME_H
+
+#include <iostream>
+#include <string>
+
+const int N_STACK = 100; 
+
+struct Stack {
+    std::string info[N_STACK]; 
+    int top;
+};
+
+void create_stack(Stack &S);
+bool is_full(const Stack &S); 
+bool is_empty(const Stack &S); 
+void push(Stack &S, const std::string &x); 
+void pop(Stack &S, std::string &p);
+void kelolaAksi(const std::string &aksiPemain, Stack &StackAksi, Stack &StackRedo);
+void hitungTotalDamage(Stack &S, int &totalDamage);
+
+#endif
